31eb13d52fce93776d01b3a30b0674a1759ee65f
UlanDT <ulanbek.dt94@gmail.com>
2020-09-09 17:39:30 +0600

    updated files

--- namba/namba/items.py
+++ namba/namba/items.py
@@ -11,6 +11,16 @@ import scrapy
 class NambaItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    # cafe_name = scrapy.Field()
-    # cafe_image = scrapy.Field()
-    pass
+
+    cafe_name = scrapy.Field()
+    cafe_image_url = scrapy.Field()
+    cafe_background_image_url = scrapy.Field()
+    average_price = scrapy.Field()
+    delivery_price = scrapy.Field()
+    working_hours = scrapy.Field()
+    address = scrapy.Field()
+    phone = scrapy.Field()
+    categories = scrapy.Field()
+    reviews = scrapy.Field()
+
+
--- namba/namba/spiders/nambafood.py
+++ namba/namba/spiders/nambafood.py
@@ -4,16 +4,23 @@ import scrapy
 from ..items import NambaItem
 
 
-
 class NambafoodSpider(scrapy.Spider):
 
     name = 'nambafood'
 
-    start_urls = ['https://nambafood.kg/dominopizza']
+    start_urls = ['https://nambafood.kg/cafe']
 
     def parse(self, response):
 
-        items = NambaItem
+        all_links = response.css("a.cafe-item::attr(href)").extract()
+        urls = ["https://nambafood.kg" + i for i in all_links]
+        print(len(urls))
+        for i in urls:
+            yield response.follow(i, callback=self.parse)
+
+        print(response)
+
+        items = NambaItem()
 
         cafe_name = response.css(".cafe--name::text")[0].extract().strip()
         cafe_image_url = response.css(".prev--thumb img").xpath("@src")[0].extract()
@@ -24,15 +31,18 @@ class NambafoodSpider(scrapy.Spider):
         working_hours = response.xpath("//div[contains(@class, 'information--item--description')]/text()")[3].extract().strip()
         address = response.css(".map-address--text::text")[1].extract().strip()
         phone = response.css(".map-address--text::text")[3].extract().strip()
-        # latitude = response.css("div.map a::attr(href)").extract()
+        latitude = response.css("div.map a::attr(href)").extract()
+        latitude = response.css("div.map > a")
 
+        """
+        Section for scraping restaurant menu
+        """
         spans = response.css("span.section--container")  # 13
         categories = {}
-        dish_info = {}
         for span in spans:
             category_name = span.css("h2.title::text").get().strip()
             card_items = span.css("div.card--item")
-
+            dish_info = {}
             for card_item in card_items:
 
                 if any(card_item.css("div.card--item--title::text").getall()):
@@ -41,38 +51,41 @@ class NambafoodSpider(scrapy.Spider):
                     continue
 
                 dish_price = card_item.css("div.price::text").extract()[0].strip()
+                dish_image_url = card_item.css('.card--item--prev img::attr(src)').extract_first()
+                dish_description = card_item.css(".card--item--description::text")[0].extract().strip()
+                dish_info.update({dish_title: {'dish_price': dish_price,
+                                               "dish_image_url": "https://nambafood.kg"+dish_image_url,
+                                               "dish_description": dish_description}})
+            categories.update({category_name: dish_info})
 
-                dish_info.update({category_name: {dish_title: {'dish_price': dish_price}}})
-                print(dish_info)
-            # print(dish_info)
-            categories.update(dish_info)
+        """
+        Section to scrape reviews
+        """
+        all_reviews = response.css(".feedback--comment-wrap")
+        reviews = {}
 
+        for review in all_reviews:
+            try:
+                reviewed_user = review.css("span.name::text")[0].extract()
+                reviewed_date = review.css("span.date::text")[0].extract()
+                review_text = review.css("div.comment--text::text")[0].extract().strip()
 
-        yield ({"cafe_name": cafe_name,
-                "cafe_image_url": "https://nambafood.kg" + cafe_image_url,
-                "cafe_background_image_url": cafe_background_image_url,
-                "average_price": average_price,
-                "delivery_price": delivery_price,
-                "working_hours": working_hours,
-                "address": address,
-                "phone": phone,
-                "latitude": "latitude",
-                "categories": categories})
-        # items['cafe_name'] = cafe_name
-        # items['cafe_image'] = cafe_image
-        #
-        # yield items
-        # all_restaurant_titles = response.css('div.cafe--name::text').extract()
-        #
-        # items['title'] = all_restaurant_titles
-        # # items['author'] = author
-        # yield {'restaurant_title': all_restaurant_titles}
+                reviews.update({"reviewed_user": reviewed_user,
+                                "reviewed_date": reviewed_date,
+                                "review_text": review_text})
+            except IndexError:
+                continue
 
+        items['cafe_name'] = cafe_name
+        items['cafe_image_url'] = cafe_image_url
+        items['cafe_background_image_url'] = cafe_background_image_url
+        items['average_price'] = average_price
+        items['delivery_price'] = delivery_price
+        items['working_hours'] = working_hours
+        items['address'] = address
+        items['phone'] = phone
+        items['categories'] = categories
+        items['reviews'] = reviews
 
+        yield items
 
-        # yield response.follow(links, callback=_self.parse)
-        # yield items
-        # scrapy crawl quotes -o items.json save them in json file
-# response.xpath("//div[@class='cafe--name']/text()")[0].extract()
-# response.css("div.catalog-wrap a").xpath("@href").extract()
-# response.css("a").xpath("@href").extract()  all links
